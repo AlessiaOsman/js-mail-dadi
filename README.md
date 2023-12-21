5e08a0150b7e3b6f06992d1fc75a06c06c8f8181
AlessiaOsman <alessia.osman@gmail.com>
2023-12-21 16:44:56 +0100

    Preparazione elementi

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 - random per il giocatore
 - condizione per numero più alto
 - stampo in pagina numeri e vincitore
+
+
+const computerNumber = Math.floor(Math.random()*6 + 1)
+console.log(computerNumber)
+
+const userNumber = Math.floor(Math.random()*6 + 1)
+console.log(userNumber)
---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*Recupero elementi */
+const computerParagraph = document.getElementById('computer');
+console.log(computerParagraph);
+
+const userParagraph = document.getElementById('user');
+console.log(userParagraph);
+
+const winnerParagraph = document.getElementById('winner');
+console.log(winnerParagraph);
+
+const button = document.getElementById('dice-button');
+console.log(button);
+
+textNumber = "E' uscito il numero "
+textWinner = "Ha vinto "
+
+
