845a605975ff46250d4bdbad87ce513c698962e5
AlessiaOsman <alessia.osman@gmail.com>
2023-12-22 00:43:54 +0100

    EventListener, ciclo for e condizione

--- js/script_mail.js
+++ js/script_mail.js
@@ -16,3 +16,20 @@ console.log(inputEmail)
 const checkButton = document.getElementById('check-input')
 console.log(checkButton)
 
+const errorAlert = document.getElementById('error-alert')
+console.log(errorAlert)
+const successAlert = document.getElementById('success-alert')
+console.log(successAlert)
+
+const userEmail = inputEmail.value.trim()
+
+checkButton.addEventListener ('click', function (){
+
+    for (let i = 0; i<=3; i++){
+        if (userEmail === mailList[i]){
+            successAlert.classList.remove('d-none')
+        } else if (userEmail !== mailList[i]) {
+            errorAlert.classList.remove('d-none')
+        }
+    } 
+})
